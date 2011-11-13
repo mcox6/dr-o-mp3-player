6a3f188330d3dc70301fa2c78d090697785f86b0
epicmomentoftruth <epicmomentoftruth@gmail.com>
2011-11-13 04:05:26 +0000

    I'm going to try some crazy stuff to find why the mp3s aren't playing, so I'm making this branch.

---
+++ lpc2148_FreeRTOS_v6_SD_MP3/Release/FreeRTOS/list.d
@@ -0,0 +1,23 @@
+FreeRTOS/list.d FreeRTOS/list.o: ../FreeRTOS/list.c \
+  ../FreeRTOS/FreeRTOS.h ../FreeRTOS/projdefs.h \
+  ../FreeRTOS/FreeRTOSConfig.h ../FreeRTOS/../System/sysConfig.h \
+  ../FreeRTOS/../System/lpc214x.h ../FreeRTOS/portable.h \
+  ../FreeRTOS/portmacro.h ../FreeRTOS/mpu_wrappers.h ../FreeRTOS/list.h
+
+../FreeRTOS/FreeRTOS.h:
+
+../FreeRTOS/projdefs.h:
+
+../FreeRTOS/FreeRTOSConfig.h:
+
+../FreeRTOS/../System/sysConfig.h:
+
+../FreeRTOS/../System/lpc214x.h:
+
+../FreeRTOS/portable.h:
+
+../FreeRTOS/portmacro.h:
+
+../FreeRTOS/mpu_wrappers.h:
+
+../FreeRTOS/list.h:
---
+++ lpc2148_FreeRTOS_v6_SD_MP3/Release/FreeRTOS/tasks.d
@@ -0,0 +1,28 @@
+FreeRTOS/tasks.d FreeRTOS/tasks.o: ../FreeRTOS/tasks.c \
+  ../FreeRTOS/FreeRTOS.h ../FreeRTOS/projdefs.h \
+  ../FreeRTOS/FreeRTOSConfig.h ../FreeRTOS/../System/sysConfig.h \
+  ../FreeRTOS/../System/lpc214x.h ../FreeRTOS/portable.h \
+  ../FreeRTOS/portmacro.h ../FreeRTOS/mpu_wrappers.h ../FreeRTOS/task.h \
+  ../FreeRTOS/list.h ../FreeRTOS/StackMacros.h
+
+../FreeRTOS/FreeRTOS.h:
+
+../FreeRTOS/projdefs.h:
+
+../FreeRTOS/FreeRTOSConfig.h:
+
+../FreeRTOS/../System/sysConfig.h:
+
+../FreeRTOS/../System/lpc214x.h:
+
+../FreeRTOS/portable.h:
+
+../FreeRTOS/portmacro.h:
+
+../FreeRTOS/mpu_wrappers.h:
+
+../FreeRTOS/task.h:
+
+../FreeRTOS/list.h:
+
+../FreeRTOS/StackMacros.h:
